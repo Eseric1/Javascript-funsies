1b69fadeb5dc13299629863cf013655d53ea3de1
eric samano <ericsamano48@gmail.com>
2023-04-27 18:12:22 -0700

    created starting files

---
+++ basicFunctions.js
@@ -0,0 +1,13 @@
+//Functions for basicFunctions.html
+        
+        function addNumbers() {
+            var firstNumber = parseFloat(document.getElementById("num1").value);
+            var secondNumber = parseFloat(document.getElementById("num2").value);
+            var sum = firstNumber +  secondNumber;
+            
+            alert("The sum is: " + sum);    
+        }
+        
+        
+        // add the rest of your functions below...
+   
